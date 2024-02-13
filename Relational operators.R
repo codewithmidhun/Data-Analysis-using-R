7f6a88fe33be5d4d86617c0c9c47dbf3ea49e831
MIDHUN SUNIL <126195984+codewithmidhun@users.noreply.github.com>
2024-02-13 16:09:41 +0530

    Add files via upload

---
+++ Relational operators.R
@@ -0,0 +1,11 @@
+# R program to illustrate 
+# the use of Relational operators 
+vec1 <- c(0, 2) 
+vec2 <- c(2, 3) 
+
+# Performing operations on Operands 
+cat ("Vector1 less than Vector2 :", vec1 < vec2, "\n") 
+cat ("Vector1 less than equal to Vector2 :", vec1 <= vec2, "\n") 
+cat ("Vector1 greater than Vector2 :", vec1 > vec2, "\n") 
+cat ("Vector1 greater than equal to Vector2 :", vec1 >= vec2, "\n") 
+cat ("Vector1 not equal to Vector2 :", vec1 != vec2, "\n") 
