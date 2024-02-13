6968535d4ae52472d44b9e46a8b55b0dc5b13070
MIDHUN SUNIL <126195984+codewithmidhun@users.noreply.github.com>
2024-02-13 17:03:44 +0530

    Add files via upload

---
+++ Listing the Variables.R
@@ -0,0 +1,11 @@
+#Listing the variables
+myRealNumeric <- 10
+myDecimalNumeric <- 10.0
+myCharacter <- "10"
+myBoolean <- TRUE
+myInteger <- 0:10
+myComplex <- 5i
+cat("Variables in the current directory: \n")
+ls() # returns all the variables created in the workspace alphabetically
+cat("\n")
+
