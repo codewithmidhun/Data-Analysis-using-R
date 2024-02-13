52288ea8f20a006bf4eb7733bdb9f799154d7962
MIDHUN SUNIL <126195984+codewithmidhun@users.noreply.github.com>
2024-02-13 16:11:08 +0530

    Add files via upload

---
+++ Assignment operators.R
@@ -0,0 +1,14 @@
+# R program to illustrate 
+# the use of Assignment operators 
+vec1 <- c(2:5) 
+c(2:5) ->> vec2 
+vec3 <<- c(2:5) 
+vec4 = c(2:5) 
+c(2:5) -> vec5 
+
+# Performing operations on Operands 
+cat ("vector 1 :", vec1, "\n") 
+cat("vector 2 :", vec2, "\n") 
+cat ("vector 3 :", vec3, "\n") 
+cat("vector 4 :", vec4, "\n") 
+cat("vector 5 :", vec5) 
