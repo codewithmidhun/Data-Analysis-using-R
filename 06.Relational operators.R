8688d15cc4482effcaa300a7fe8b688cf3d31c68
MIDHUN SUNIL <126195984+codewithmidhun@users.noreply.github.com>
2024-02-23 19:15:14 -0800

    06.Relational operators.R

--- 06.Relational operators.R
+++ 06.Relational operators.R
@@ -1,3 +1,36 @@
+#Each element of the 1st vector is compared with the corresponding element of the 2nd vector. 
+#The result of comparison is a Boolean value.
+
+#(>)
+v <- c(2,5.5,6,9)
+t <- c(8,2.5,14,9)
+print(v > t)
+
+#(<)
+v <- c(2,5.5,6,9)
+t <- c(8,2.5,14,9)
+print(v < t)
+
+#(==)
+v <- c(2,5.5,6,9)
+t <- c(8,2.5,14,9)
+print(v == t)
+
+#(<=)
+v <- c(2,5.5,6,9)
+t <- c(8,2.5,14,9)
+print(v <= t)
+
+#(v >= t)
+v <- c(2,5.5,6,9)
+t <- c(8,2.5,14,9)
+print(v >= t)
+
+#(v != t)
+v <- c(2,5.5,6,9)
+t <- c(8,2.5,14,9)
+print(v != t)
+
 # R program to illustrate 
 # the use of Relational operators 
 vec1 <- c(0, 2) 
