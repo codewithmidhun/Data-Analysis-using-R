54bb25840b73b3ebe7aefb9842eff9dd70d8e8be
MIDHUN SUNIL <126195984+codewithmidhun@users.noreply.github.com>
2024-02-23 18:36:17 -0800

    04.Arithmetic operators.R

--- 04.Arithmetic operators.R
+++ 04.Arithmetic operators.R
@@ -1,3 +1,38 @@
+#add (+) 2 vectors
+v <- c( 2,5.5,6)
+t <- c(8, 3, 4)
+print(v+t)
+
+#sub (-) 2nd vector from 1st
+v <- c( 2,5.5,6)
+t <- c(8, 3, 4)
+print(v-t)
+
+#multiply (*)both vectors
+v <- c( 2,5.5,6)
+t <- c(8, 3, 4)
+print(v*t)
+
+#divide 1st vector with 2nd
+v <- c( 2,5.5,6)
+t <- c(8, 3, 4)
+print(v/t)
+
+#remainder of the first vector with the second (%%)
+v <- c( 2,5.5,6)
+t <- c(8, 3, 4)
+print(v%%t)
+
+#result of division of first vector with second (quotient - %/%)
+v <- c( 2,5.5,6)
+t <- c(8, 3, 4)
+print(v%/%t)
+
+#first vector raised to the exponent of second vector (power - ^) 
+v <- c( 2,5.5,6)
+t <- c(8, 3, 4)
+print(v^t)
+
 # R program to illustrate 
 # the use of Arithmetic operators 
 vec1 <- c(0, 2) 
