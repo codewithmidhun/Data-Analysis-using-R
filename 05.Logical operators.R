a72a0bf33f5c88c357535aaeb20c536623844e3f
MIDHUN SUNIL <126195984+codewithmidhun@users.noreply.github.com>
2024-02-27 00:53:53 -0800

    05.Logical operators.R

--- 05.Logical operators.R
+++ 05.Logical operators.R
@@ -1,3 +1,39 @@
+# element wise & 
+a=c(2,3,4)
+b=c(4,3,1)
+print(a&b)
+m=3
+n=2
+print(m&n)
+
+# element wise OR (|) 
+a=c(2,0,4)
+b=c(4,0,1)
+print(a|b)
+
+m=3
+n=2
+print(m|n)
+
+# Logical Not (!)
+a=c(2,0,4)
+b=c(4,0,1)
+print(!a)
+
+m=0
+n=3
+print(!m)
+
+#Logical & and Logical OR
+  
+m = 0
+n = 2
+p =10
+q = 2
+print((m>n)&&(p>q))
+print((m>n)||(p>q))
+
+
 # R program to illustrate 
 # the use of Logical operators 
 vec1 <- c(0,2) 
