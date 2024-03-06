4cad3c314c5b38c4268414cd6bdd71748883b323
MIDHUN SUNIL <126195984+codewithmidhun@users.noreply.github.com>
2024-03-05 20:29:49 -0800

    08.Miscellaneous operators.R

--- 08.Miscellaneous operators.R
+++ 08.Miscellaneous operators.R
@@ -1,3 +1,22 @@
+#: Range ; a = 1:5 ;a= 1 2 3 4 5
+a<-1:5
+print(a)
+
+a <-letters[1:26]
+print(a)
+
+#%ini% search whether b in a
+a<-1:5
+b<- 7
+print(b %in% a)
+
+#%*% ->> multiply a matrix with its transpose
+
+a=c(1,2,3)
+b=c(2,3,4)
+#(1*2)+(2*3)+(3*4)=20
+print(a %*% b)
+
 # R program to illustrate 
 # the use of Miscellaneous operators 
 mat <- matrix (1:4, nrow = 1, ncol = 4) 
